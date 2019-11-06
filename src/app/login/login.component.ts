8e9a8e435d8c50e0ff4e746d37a33789b607e45f
LucasWatanuki <lucas.watanuki@ibm.com>
2019-11-06 12:10:37 -0300

    url update

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -58,6 +58,6 @@ export class LoginComponent implements OnInit {
   }
 
   reloadPage() {
-    window.location.replace('http://localhost:4200/home');
+    window.location.replace('https://migsoft.herokuapp.com/home');
   }
 }
